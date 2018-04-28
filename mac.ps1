2f20ed6d9e88456d13aecb5d8b733bd9c53c5c14
neil <neil@fifteen-mbp-1.local>
2018-04-28 23:47:14 +0100

    added some comments

--- mac.ps1
+++ mac.ps1
@@ -1,6 +1,11 @@
+# Get current PowerShell version
 $PSVersion = $PSVersionTable.PSVersion
+
+# Output something
 Write-Host "Testing out some PowerShell ($PSVersion) on MacOS."
 Write-Host "Is this MacOS? " -NoNewline
+
+# Am I running on MacOS? y/n
 if ($IsMacOS) {
     Write-Host "Yes" -ForegroundColor "Green"
 } else {
